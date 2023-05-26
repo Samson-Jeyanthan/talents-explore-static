60e9d5aa04c27bb6815f0eb6e50deac03b9fd670
Samson <samsonjeyanthan5@gmail.com>
2023-05-26 11:00:05 +0530

    Changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  About,
-  Hero,
-  ImgSection1,
-  ImgSection2,
-  PolicyCenter,
-  VM,
-} from "../Contents";
+import { About, Hero, ImgSection1, PolicyCenter, VM } from "../Contents";
 import { HeroNavbar } from "../Components";
 const Home = () => {
   return (
@@ -16,7 +9,6 @@ const Home = () => {
       <About />
       <ImgSection1 />
       <VM />
-      {/* <ImgSection2 /> */}
       <PolicyCenter />
     </>
   );
